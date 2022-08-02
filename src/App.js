0f2ba30488a94b8fadb958ab5d53b199844a1c9e
seyoungjoy <85421850+seyoungjoy@users.noreply.github.com>
2022-08-02 10:29:31 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+import Template from "./components/Template";
+
+const App = () => {
+    return (
+        <Template>안녕하ddd세요</Template>
+    )
+}
+export default App;
